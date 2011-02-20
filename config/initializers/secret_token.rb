566d47787f5fd3aa65801b24a8069a9f2c8b9f97
Xyberrune <elliott@seventhdaystudio.com>
2011-02-20 23:39:55 +0300

    new file: .gitignore 	new file: Gemfile
    	new file: README
    	new file: Rakefile
    	new file: app/controllers/application_controller.rb
    	new file: app/helpers/application_helper.rb
    	new file: app/views/layouts/application.html.erb
      	new file: config.ru
    	new file: config/application.rb
    	new file: config/boot.rb
    	new file: config/database.yml
    	new file: config/environment.rb
    	new file: config/environments/development.rb
    	new file: config/environments/production.rb
    	new file: config/environments/test.rb
    	new file: config/initializers/backtrace_silencers.rb
    	new file: config/initializers/inflections.rb
    	new file: config/initializers/mime_types.rb
    	new file: config/initializers/secret_token.rb
    	new file: config/initializers/session_store.rb
    	new file: config/locales/en.yml
    	new file: config/routes.rb
    	new file: db/seeds.rb
    	new file: doc/README_FOR_APP
    	new file: lib/tasks/.gitkeep
    	new file: public/404.html
    	new file: public/422.html
    	new file: public/500.html
    	new file: public/favicon.ico
    	new file: public/images/rails.png
    	new file: public/index.html
    	new file: public/javascripts/application.js
    	new file: public/javascripts/controls.js
    	new file: public/javascripts/dragdrop.js
    	new file: public/javascripts/effects.js
    	new file: public/javascripts/prototype.js
    	new file: public/javascripts/rails.js
    	new file: public/robots.txt
    	new file: public/stylesheets/.gitkeep
    	new file: script/rails
    	new file: test/performance/browsing_test.rb
    	new file: test/test_helper.rb
    	new file: vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '045403cb703abc990678b56cc7b66eb59125fe7b920bbd0293f73b8da6abd2b9b47c5dd46b0bbe02728983ee6cd04ba8d4a403887f9f6e64fa0099ea4f5121f1'
